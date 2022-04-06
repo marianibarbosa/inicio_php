e7f52a02ea5d4a3f92ec7110a7308eddbf65bf59
marianibarbosa <98926896+marianibarbosa@users.noreply.github.com>
2022-04-05 21:49:47 -0300

    Add files via upload

---
+++ lib/listaAlunos.php
@@ -0,0 +1,25 @@
+<?php 
+   
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HOME</title>
+</head>
+<body>
+    <header>
+        <a href="/"> HOME </a>
+        <a href="/04-04-22/lib/listaAlunos.php"> Alunos </a>
+    </header>
+    <main>
+        <ul>
+
+        </ul>
+    </main>
+    
+</body>
+</html>
---
+++ lib/listar.php
@@ -0,0 +1,11 @@
+<?php 
+    $alunos = ['Bianca', 'Claudia', 'Luiza', 'Marcos', 'Mariani', 'Michele', 'Pedro', 'Sávio'];
+    
+
+function listaAlunos()
+{
+    $alunos_local = $GLOBALS['alunos'];
+    return $GLOBALS['alunos'];
+}
+
+?>
